374e58fa41642586a859d1c236eb4c8855d11e84
Carlos Eduardo <caducarlos1109@gmail.com>
2024-08-07 22:04:03 -0300

    Arrumando Bug alerta infinio teste vocacional

--- script.js
+++ script.js
@@ -1,16 +1,24 @@
 function submitTest() {
     var respostas = [];
     var sections = document.querySelectorAll('.question');
+    var allAnswered = true
 
     sections.forEach(function(section) {
         var resposta = section.querySelector('input[type="radio"]:checked');
         if (resposta) {
             respostas.push(resposta.value);
         } else {
-            alert("Por favor, responda todas as perguntas para continuar.");
+           // alert("Por favor, responda todas as perguntas para continuar.");
+           allAnswered = false;
             return;
         }
     });
+
+    //Faz aparecer o alerta 1 vez
+    if (!allAnswered) {
+        alert("Por favor, responda todas as perguntas para continuar.");
+        return;
+    }
 
     if (respostas.length === sections.length) {
         var countHumanas = 0;
@@ -32,9 +40,9 @@ function submitTest() {
         // Formatar para exibir uma mensagem
         var resultado = "";
         if (porcentagemHumanas > porcentagemExatas) {
-            resultado = "Você parece ter afinidade com áreas de Humanas.";
+            resultado = "Você parece ter afinidade com áreas de Humanas.<br><br>Você é de humanas!<br><br>Pessoas com ese perfil geralmente tem interese com temas que envolvem compreensão do comportameno humano, a cultura e a sociedade. Elas se destacam em áreas como Piscologia, Direito, Comunicação, Historia, Filosoia e Ciencias sociais. Gostam de refletir sobre temas variados e tem uma boa capacidade de argumentação e persuasão.";
         } else if (porcentagemExatas > porcentagemHumanas) {
-            resultado = "Você parece ter afinidade com áreas de Exatas.<br>";
+            resultado = "Você parece ter afinidade com áreas de Exatas.<br><br>Você é de Exatas!<br><br>Pessoas com ese perfil tem uma inclinação para resolver problemaspráticos e logicos elas se destacam em áreascomo Engenharia, Matematica, Fisica, Economia, Informatica. Tendem a ser metodicas, analiticas e gostam de desafios que envolvem raciocinio logico e precisão";
         } else {
             resultado = "Você possui características de ambas as áreas. Considere explorar mais sobre cada uma delas.";
         }
